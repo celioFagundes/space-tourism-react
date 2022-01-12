a4541090ffbd992e64384b5b59a4b86d916d9d51
Célio <celiofagundes30@gmail.com>
2022-01-12 10:40:07 -0300

    responsive home styles

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,25 +2,23 @@ import React from 'react'
 import styles from './style.module.css'
 import { Link } from 'react-router-dom'
 const Home = () => {
-  
   return (
-    
-    <div className = {styles.wrapper}>
-        <div className={styles.container}>
-          <div className={styles.container_intro}>
-            <p>So, you want to travel to </p>
-            <p>Space</p>
-            <p>
-              Let’s face it; if you want to go to space, you might as well genuinely go to outer space
-              and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a
-              truly out of this world experience!
-            </p>
-          </div>
-          <Link to = '/destination/moon'className={styles.explore}>
-            <div className={styles.exploreGrow}></div>
-              <p className={styles.explore_text}>Explore</p>
-          </Link>
-    </div>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.container_intro}>
+          <p>So, you want to travel to </p>
+          <p>Space</p>
+          <p>
+            Let’s face it; if you want to go to space, you might as well genuinely go to outer space
+            and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you
+            a truly out of this world experience!
+          </p>
+        </div>
+        <Link to='/destination/moon' className={styles.explore}>
+          <div className={styles.exploreGrow}></div>
+          <p className={styles.explore_text}>Explore</p>
+        </Link>
+      </div>
     </div>
   )
 }
